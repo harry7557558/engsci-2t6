fix flexural shear calculation

civ102-project/bridge_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import beam_analysis
 import cross_section_analysis as csa
@@ -60,7 +61,7 @@ def get_max_bend(x0, x1):
 
 class BridgeCrossSection:
 
-    def __init__(self, label: str, parts, glues, x0, x1):
+    def __init__(self, label, parts, glues, x0, x1):
         assert x0 < x1
         self.solved = False
         self.label = label
@@ -144,20 +145,26 @@ class Bridge:
         self.params = initial_params[:]
 
     def calc_glues(self, cross_sections):
+        """Calculate glue joints 
+            set glues of the parameter to empty
+            returns list[BridgeCrossSection] with only glues
+            label is True if it is a concern of flexural shear failure"""
         for cs in cross_sections:
             cs.glues = []
         # might include duplicate glue joints
         # underestimate is better than overestimate
         res = []
         for j in range(len(cross_sections)):
+            cs2 = cross_sections[j]
             for i in range(j):
                 cs1, cs2 = cross_sections[i], cross_sections[j]
-                if cs1.x0 > cs2.x0 or (cs1.x0 == cs2.x0 and cs1.x1 > cs2.x1):
+                if (cs1.x0, cs1.x1) > (cs2.x0, cs2.x1):
                     cs1, cs2 = cs2, cs1
                 if cs2.x0 >= cs1.x1:
                     continue
                 glues = csa.intersection_pieces(cs1.parts, cs2.parts)
-                cs = BridgeCrossSection('', [], glues, cs2.x0, cs1.x1)
+                cs = BridgeCrossSection(cs1.x1 <= cs2.x1, [], glues,
+                                        cs2.x0, min(cs1.x1, cs2.x1))
                 res.append(cs)
         return res
 
@@ -239,7 +246,10 @@ class Bridge:
             fos_flexshear = float('inf')
             peri, (xc, yc), (Ix, I) = csa.calc_geometry(parts)
             for glue in glues:
-                if glue.x0 >= x1 or glue.x1 <= x0:
+                if glue.label == False:
+                    continue
+                if not glue.x0 <= x0 < x1 <= glue.x1:
+                    assert x0 >= glue.x1 or x1 <= glue.x0
                     continue
                 for (p1, p2) in glue.glues:
                     maxy = max(abs(p1[1]-yc), abs(p2[1]-yc))
@@ -257,7 +267,7 @@ class Bridge:
                 fos_bend_buckle /= 3
                 fos_shear /= 1.5
                 fos_shear_buckle /= 3
-                fos_flexshear /= 5
+                fos_flexshear /= 40  # don't think I calculated this one correctly
             min_fos = min(min_fos,
                           fos_bend, fos_bend_buckle,
                           fos_shear, fos_shear_buckle,
@@ -273,17 +283,18 @@ class Bridge:
                 c_bend_buckle = fos_bend_buckle * csa.BM_MAX / bms
                 c_shear = fos_shear * csa.SHEAR_MAX / sfs
                 c_shear_buckle = fos_shear_buckle * csa.SHEAR_MAX / sfs
+                c_flexshear = fos_flexshear * csa.BM_MAX / bms
                 cs_x += xs.tolist()
                 cs_bend += c_bend.tolist()
                 cs_bend_buckle += c_bend_buckle.tolist()
                 cs_shear += c_shear.tolist()
                 cs_shear_buckle += c_shear_buckle.tolist()
-                cs_flexshear += (fos_flexshear*one).tolist()
+                cs_flexshear += c_flexshear.tolist()
 
         if plot:
             fig, (ax1, ax2) = plt.subplots(2, 1)
             one = np.float64(1)
-            ax1.plot(cs_x, one/cs_bend, label="bend fos⁻¹")
+            ax1.plot(cs_x, one/cs_bend, label="flexural fos⁻¹")
             ax1.plot(cs_x, one/cs_bend_buckle, label="bend buckle fos⁻¹")
             ax1.plot(cs_x, one/cs_shear, label="shear fos⁻¹")
             ax1.plot(cs_x, one/cs_shear_buckle, label="shear buckle fos⁻¹")

civ102-project/cross_section_analysis.py
@@ -174,7 +174,7 @@ def calc_geometry(parts):
 def overlap_pieces(pieces0):
     """Divide/merge overlapping pieces"""
     pieces0.sort(key=lambda ps: np.linalg.norm(ps[1]-ps[0]))
-    colinears = []
+    colinears = []  # n, d, pieces
     ok = lambda n, d, p: abs(n[0]*p[0]+n[1]*p[1]-d) < 1e-6
     for (p1, p2) in pieces0:
         n = (-(p2[1]-p1[1]), p2[0]-p1[0])
@@ -207,32 +207,49 @@ def overlap_pieces(pieces0):
 
 
 def intersection_pieces(pieces1, pieces2):
-    """Find the part shared by both pieces
-        Result may include duplicates
-        Very likely has a bug but I'm too lazy to fix it
-            because it does not affect the result much"""
+    """Find the parts shared by both pieces"""
     pieces1 = sum([list(zip(p[:-1], p[1:])) for p in pieces1], [])
+    pieces1.sort(key=lambda ps: np.linalg.norm(ps[1]-ps[0]))
     pieces2 = sum([list(zip(p[:-1], p[1:])) for p in pieces2], [])
-    def hashp(p):
-        s = 1.3*np.sin(p[0]+0.2)+0.2*np.cos(0.5-p[1])
-        h = np.hypot(p[0], p[1])+1
-        w = (p[0]/h+1.2)*np.tanh(p[1]/h-0.7)
-        return s + w
-    def hashs(s):
-        h1, h2 = hashp(s[0]), hashp(s[1])
-        return tuple(sorted([h1, h2]))
-    def pieces2set(pieces):
-        res = set()
-        for i in range(len(pieces)):
-            s = list(pieces[i])
-            res.add(hashs(s))
-        return res
-    set1, set2 = pieces2set(pieces1), pieces2set(pieces2)
-    res = []
-    for piece in pieces2:
-        if hashs(piece) in set1:
-            res.append(piece)
-    return res
+    pieces2.sort(key=lambda ps: np.linalg.norm(ps[1]-ps[0]))
+    # get colinear lines
+    colinears = []  # n, d, pieces
+    ok = lambda n, d, p: abs(n[0]*p[0]+n[1]*p[1]-d) < 1e-6
+    def add_pieces(pieces, pid):
+        for (p1, p2) in pieces:
+            n = (-(p2[1]-p1[1]), p2[0]-p1[0])
+            n = (n[0]/np.hypot(n[0],n[1]), n[1]/np.hypot(n[0],n[1]))
+            d = n[0]*p1[0] + n[1]*p1[1]
+            colinears.append([n, d, [[p1, p2, pid]]])
+            for i in range(len(colinears)-1):
+                n, d, pieces = colinears[i]
+                if ok(n, d, p1) and ok(n, d, p2):
+                    pieces.append([p1, p2, pid])
+                    del colinears[-1]
+                    break
+    add_pieces(pieces1, 0)
+    add_pieces(pieces2, 1)
+    # check each straight line
+    pieces = []  # p1, p2
+    for (n, d, pairs) in colinears:
+        comp = lambda p: n[0]*p[1]-n[1]*p[0]
+        ps = []  # p, increment, pid
+        for (p1, p2, pid) in pairs:
+            if comp(p1) > comp(p2):
+                p1, p2 = p2, p1
+            ps += [(p1, 1, pid), (p2, -1, pid)]
+        ps.sort(key=lambda p: (comp(p[0]), p[2]))
+        count = [0, 0]
+        for ((p1, d1, pid1), (p2, d2, pid2)) in zip(ps[:-1], ps[1:]):
+            count[pid1] += d1
+            assert min(count) >= 0
+            #assert comp(p2) >= comp(p1)
+            if comp(p2)-comp(p1) < 1e-6:
+                continue
+            if min(count) > 0:
+                pieces.append((p1, p2))
+        assert count == [0, 1] or count == [1, 0]
+    return pieces
 
 
 def cross_section_range(parts):